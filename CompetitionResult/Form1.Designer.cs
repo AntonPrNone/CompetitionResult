248f136186a8c3bd199e2cea3ab6615bba0036ee
Антон Ахтямов <yznikfortyni@gmail.com>
2021-11-08 10:17:15 +0300

    Добавьте файлы проекта.

---
+++ CompetitionResult/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace CompetitionResult
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.NumberInc = new System.Windows.Forms.MaskedTextBox();
+            this.SaveAndCloseButton = new System.Windows.Forms.Button();
+            this.DateEndPeriod = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DateStartPeriod = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PutButton = new System.Windows.Forms.Button();
+            this.DurationCall = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Text_IncomingCall = new System.Windows.Forms.Label();
+            this.TimeConnection = new System.Windows.Forms.MaskedTextBox();
+            this.DateConnection = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NumberOut = new System.Windows.Forms.MaskedTextBox();
+            this.Text_OutgoingCall = new System.Windows.Forms.Label();
+            this.SaveReportButton = new System.Windows.Forms.Button();
+            this.OpenFileButton = new System.Windows.Forms.Button();
+            this.Heading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // NumberInc
+            // 
+            this.NumberInc.Location = new System.Drawing.Point(668, 168);
+            this.NumberInc.Mask = "+7(999) 000-000";
+            this.NumberInc.Name = "NumberInc";
+            this.NumberInc.Size = new System.Drawing.Size(100, 20);
+            this.NumberInc.TabIndex = 54;
+            // 
+            // SaveAndCloseButton
+            // 
+            this.SaveAndCloseButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.SaveAndCloseButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SaveAndCloseButton.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SaveAndCloseButton.Location = new System.Drawing.Point(356, 424);
+            this.SaveAndCloseButton.Name = "SaveAndCloseButton";
+            this.SaveAndCloseButton.Size = new System.Drawing.Size(108, 27);
+            this.SaveAndCloseButton.TabIndex = 53;
+            this.SaveAndCloseButton.Text = "Save and Close";
+            this.SaveAndCloseButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SaveAndCloseButton.UseVisualStyleBackColor = false;
+            this.SaveAndCloseButton.Click += new System.EventHandler(this.SaveAndCloseButton_Click);
+            // 
+            // DateEndPeriod
+            // 
+            this.DateEndPeriod.Location = new System.Drawing.Point(638, 381);
+            this.DateEndPeriod.Name = "DateEndPeriod";
+            this.DateEndPeriod.Size = new System.Drawing.Size(140, 20);
+            this.DateEndPeriod.TabIndex = 52;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(468, 381);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 18);
+            this.label4.TabIndex = 51;
+            this.label4.Text = "Period end date:";
+            // 
+            // DateStartPeriod
+            // 
+            this.DateStartPeriod.Location = new System.Drawing.Point(237, 379);
+            this.DateStartPeriod.Name = "DateStartPeriod";
+            this.DateStartPeriod.Size = new System.Drawing.Size(140, 20);
+            this.DateStartPeriod.TabIndex = 50;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(19, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 18);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "Start date of the period:";
+            // 
+            // PutButton
+            // 
+            this.PutButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PutButton.Location = new System.Drawing.Point(356, 229);
+            this.PutButton.Name = "PutButton";
+            this.PutButton.Size = new System.Drawing.Size(75, 23);
+            this.PutButton.TabIndex = 48;
+            this.PutButton.Text = "Put";
+            this.PutButton.UseVisualStyleBackColor = true;
+            this.PutButton.Click += new System.EventHandler(this.PutButton_Click);
+            // 
+            // DurationCall
+            // 
+            this.DurationCall.Location = new System.Drawing.Point(362, 196);
+            this.DurationCall.Name = "DurationCall";
+            this.DurationCall.Size = new System.Drawing.Size(60, 20);
+            this.DurationCall.TabIndex = 47;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(318, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 18);
+            this.label2.TabIndex = 46;
+            this.label2.Text = "Connection duration";
+            // 
+            // Text_IncomingCall
+            // 
+            this.Text_IncomingCall.AutoSize = true;
+            this.Text_IncomingCall.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Text_IncomingCall.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text_IncomingCall.ForeColor = System.Drawing.SystemColors.Control;
+            this.Text_IncomingCall.Location = new System.Drawing.Point(665, 129);
+            this.Text_IncomingCall.Name = "Text_IncomingCall";
+            this.Text_IncomingCall.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text_IncomingCall.Size = new System.Drawing.Size(104, 18);
+            this.Text_IncomingCall.TabIndex = 45;
+            this.Text_IncomingCall.Text = "Incoming call";
+            // 
+            // TimeConnection
+            // 
+            this.TimeConnection.Location = new System.Drawing.Point(471, 131);
+            this.TimeConnection.Mask = "00:00:00";
+            this.TimeConnection.Name = "TimeConnection";
+            this.TimeConnection.Size = new System.Drawing.Size(48, 20);
+            this.TimeConnection.TabIndex = 44;
+            this.TimeConnection.ValidatingType = typeof(System.DateTime);
+            // 
+            // DateConnection
+            // 
+            this.DateConnection.Location = new System.Drawing.Point(293, 130);
+            this.DateConnection.Name = "DateConnection";
+            this.DateConnection.Size = new System.Drawing.Size(140, 20);
+            this.DateConnection.TabIndex = 43;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(290, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 18);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Date/time of connection start";
+            // 
+            // NumberOut
+            // 
+            this.NumberOut.Location = new System.Drawing.Point(31, 168);
+            this.NumberOut.Mask = "+7(999) 000-000";
+            this.NumberOut.Name = "NumberOut";
+            this.NumberOut.Size = new System.Drawing.Size(100, 20);
+            this.NumberOut.TabIndex = 41;
+            // 
+            // Text_OutgoingCall
+            // 
+            this.Text_OutgoingCall.AutoSize = true;
+            this.Text_OutgoingCall.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Text_OutgoingCall.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text_OutgoingCall.ForeColor = System.Drawing.SystemColors.Control;
+            this.Text_OutgoingCall.Location = new System.Drawing.Point(28, 130);
+            this.Text_OutgoingCall.Name = "Text_OutgoingCall";
+            this.Text_OutgoingCall.Size = new System.Drawing.Size(103, 18);
+            this.Text_OutgoingCall.TabIndex = 40;
+            this.Text_OutgoingCall.Text = "Outgoing call";
+            // 
+            // SaveReportButton
+            // 
+            this.SaveReportButton.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.SaveReportButton.Location = new System.Drawing.Point(687, 37);
+            this.SaveReportButton.Name = "SaveReportButton";
+            this.SaveReportButton.Size = new System.Drawing.Size(91, 26);
+            this.SaveReportButton.TabIndex = 39;
+            this.SaveReportButton.Text = "Save Report";
+            this.SaveReportButton.UseVisualStyleBackColor = true;
+            this.SaveReportButton.Click += new System.EventHandler(this.SaveReportButton_Click);
+            // 
+            // OpenFileButton
+            // 
+            this.OpenFileButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OpenFileButton.Location = new System.Drawing.Point(22, 37);
+            this.OpenFileButton.Name = "OpenFileButton";
+            this.OpenFileButton.Size = new System.Drawing.Size(75, 26);
+            this.OpenFileButton.TabIndex = 38;
+            this.OpenFileButton.Text = "Open file";
+            this.OpenFileButton.UseVisualStyleBackColor = true;
+            this.OpenFileButton.Click += new System.EventHandler(this.OpenFileButton_Click);
+            // 
+            // Heading
+            // 
+            this.Heading.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Heading.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Heading.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Heading.ForeColor = System.Drawing.Color.Green;
+            this.Heading.Location = new System.Drawing.Point(0, 0);
+            this.Heading.Name = "Heading";
+            this.Heading.Size = new System.Drawing.Size(800, 35);
+            this.Heading.TabIndex = 37;
+            this.Heading.Text = "Spammer Detection";
+            this.Heading.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 469);
+            this.Controls.Add(this.NumberInc);
+            this.Controls.Add(this.SaveAndCloseButton);
+            this.Controls.Add(this.DateEndPeriod);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.DateStartPeriod);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PutButton);
+            this.Controls.Add(this.DurationCall);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Text_IncomingCall);
+            this.Controls.Add(this.TimeConnection);
+            this.Controls.Add(this.DateConnection);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NumberOut);
+            this.Controls.Add(this.Text_OutgoingCall);
+            this.Controls.Add(this.SaveReportButton);
+            this.Controls.Add(this.OpenFileButton);
+            this.Controls.Add(this.Heading);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MaskedTextBox NumberInc;
+        private System.Windows.Forms.Button SaveAndCloseButton;
+        private System.Windows.Forms.DateTimePicker DateEndPeriod;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker DateStartPeriod;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button PutButton;
+        private System.Windows.Forms.TextBox DurationCall;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Text_IncomingCall;
+        private System.Windows.Forms.MaskedTextBox TimeConnection;
+        private System.Windows.Forms.DateTimePicker DateConnection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox NumberOut;
+        private System.Windows.Forms.Label Text_OutgoingCall;
+        private System.Windows.Forms.Button SaveReportButton;
+        private System.Windows.Forms.Button OpenFileButton;
+        private System.Windows.Forms.Label Heading;
+    }
+}
+
---
+++ CompetitionResult/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+
+namespace CompetitionResult
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string input;
+        public string StartTime;
+        public string EndTime;
+
+        public string NumberOutPer;
+        public string NumberIncPer;
+        public string DateConnectionPer;
+        public string TimeConnectionPer;
+        public string DurationCallPer;
+
+        private void OpenFileButton_Click(object sender, EventArgs e) // При нажатии на кнопку Open File
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    input = sr.ReadToEnd();
+                    sr.Close();
+                }
+
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" + $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+
+            input += '\n';
+            input.TrimStart('\n');
+        }
+
+        private void PutButton_Click(object sender, EventArgs e) // При нажатии на кнопку Put
+        {
+            NumberOutPer = NumberOut.Text.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", "");
+            NumberIncPer = NumberInc.Text.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", "");
+            DateConnectionPer = Convert.ToString(DateConnection.Value.ToShortDateString());
+            TimeConnectionPer = Convert.ToString(TimeConnection.Text);
+            DurationCallPer = DurationCall.Text;
+
+            // Обработка исключений при неверном формате ввода
+
+            if (NumberOutPer.Length != 11)
+            {
+                Heading.ForeColor = Color.Red;
+                Heading.Text = @"Invalid field input format ""Outgoing call"" ";
+            }
+
+            else if (NumberIncPer.Length != 11)
+            {
+                Heading.ForeColor = Color.Red;
+                Heading.Text = @"Invalid field input format ""Incoming call"" ";
+            }
+
+            else if (TimeConnectionPer.Length != 8)
+            {
+                Heading.ForeColor = Color.Red;
+                Heading.Text = @"Invalid field input format ""Time of connection start"" ";
+            }
+
+            else
+            {
+                int x = 1;
+                try
+                {
+                    Convert.ToInt32(DurationCallPer);
+                }
+
+                catch
+                {
+                    x = 0;
+                    Heading.ForeColor = Color.Red;
+                    Heading.Text = @"Invalid field input format ""Connection duration"" ";
+                }
+
+                finally
+                {
+                    if (x == 1) // Если формат ввода верный
+                    {
+                        Heading.ForeColor = Color.Green;
+                        Heading.Text = "Spammer Detection";
+
+                        input += NumberOutPer + ", " + NumberIncPer + ", " + DateConnectionPer + " " + TimeConnectionPer + ", " + DurationCallPer + "\n";
+                    }
+                }
+            }
+        }
+
+        private void SaveAndCloseButton_Click(object sender, EventArgs e) // При нажатии на кнопку Save And Close
+        {
+            input = input.TrimEnd('\n');
+            var rs = new StreamWriter(openFileDialog1.FileName, false);
+            rs.WriteLine(input);
+            rs.Close();
+        }
+
+        private void SaveReportButton_Click(object sender, EventArgs e) // Запись в otchet подходящие критерию исходящие номера, их количество и суммарное время звонков
+        {
+            StartTime = Convert.ToString(DateStartPeriod.Value);
+            EndTime = Convert.ToString(DateEndPeriod.Value);
+
+            var sr = new StreamReader(openFileDialog1.FileName);
+            int lenfile = sr.ReadToEnd().Split('\n').Length;
+            sr = new StreamReader(openFileDialog1.FileName);
+
+
+            ArrayList ishod = new ArrayList();
+            ArrayList vhod = new ArrayList();
+            ArrayList time = new ArrayList();
+            ArrayList len = new ArrayList();
+
+            for (int i = 0; i < lenfile; i++)
+            {
+                string inputline = sr.ReadLine();
+                if (inputline != "\n" && inputline != "" && inputline != null)
+                {
+                    ishod.Add(inputline.Split(',')[0].Trim());
+                    vhod.Add(inputline.Split(',')[1].Trim());
+                    time.Add(DateTime.Parse(inputline.Split(',')[2].Trim()));
+                    len.Add(Int32.Parse(inputline.Split(',')[3].Trim()));
+                }
+            }
+
+            ArrayList ishodDouble = new ArrayList();
+            foreach (string i in ishod)
+            {
+                ishodDouble.Add(i);
+            }
+
+            for (int i = 0; i < time.Count; i++) // Фильтр исходящих по временному промежутку
+            {
+                if (!(DateTime.Parse(StartTime.Substring(0, 10)) <= (DateTime)time[i] && DateTime.Parse(EndTime.Substring(0, 10)) >= (DateTime)time[i]))
+                {
+                    ishodDouble[i] = "";
+                }
+            }
+
+            for (int i = 0; i < ishod.Count; i++)
+            {
+                ishodDouble.Remove("");
+            }
+
+            Dictionary<string, int> countOfItems = new Dictionary<string, int>(); // Количество исходящих вызовов каждого номера в заданный временной промежуток
+            foreach (string eachNumber in ishodDouble)
+            {
+                if (countOfItems.ContainsKey(eachNumber))
+                    countOfItems[eachNumber]++;
+                else
+                    countOfItems[eachNumber] = 1;
+            }
+
+            Dictionary<string, int> countOfItems0 = new Dictionary<string, int>(); // Сортировка словаря по количеству исходящих вызовов каждого номера в заданный временной промежуток
+
+            foreach (var i in countOfItems.OrderByDescending(u => u.Value))
+            {
+                countOfItems0[i.Key] = i.Value;
+            }
+
+            ArrayList filteredNumber = new ArrayList(); // До десяти исходящих номеров, с которых было совершенно наибольшее количество звонков в заданный пользователем период
+            foreach (var i in countOfItems0.Keys)
+            {
+                if (filteredNumber.Count != 10)
+                {
+                    filteredNumber.Add(i);
+                }
+
+                else
+                {
+                    break;
+                }
+            }
+
+            string otchet = "";
+            int kl = 0;
+            int summ = 0;
+            int h = 0;
+            int m = 0;
+            int s = 0;
+
+            foreach (var i in filteredNumber) // Запись в otchet подходящие критерию исходящие номера, их количество и суммарное время звонков
+            {
+
+                for (int j = 0; j < ishod.Count; j++)
+                {
+                    if ((string)ishod[j] == (string)i)
+                    {
+                        kl++;
+                        summ += (int)len[j];
+                    }
+                }
+
+                h = summ / 3600;
+                m = (summ - (h * 3600)) / 60;
+                s = summ % 60;
+
+                otchet += i + ", " + kl + ", " + h + ":" + m + ":" + s + "\n";
+
+                kl = 0;
+                summ = 0;
+            }
+
+            otchet = otchet.TrimEnd('\n');
+
+            System.IO.File.WriteAllText("C:\\Users\\201904\\Documents\\Report.txt", otchet);
+        }
+    }
+}
